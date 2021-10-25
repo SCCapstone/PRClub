23c809431b059d4152388bb839457b7bc11c97d6
Ish <ishrat@cec.sc.edu>
2021-10-24 23:27:23 -0400

    Ish's branch

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Ish&apos;s Branch</Text>
       <StatusBar />
     </View>
   );
