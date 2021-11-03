b909be8ae4ba80d6aac472f2a91c1a0c13f4992b
Ish <ishrat@cec.sc.edu>
2021-11-02 20:04:36 -0400

    implement basic navigation/create project structure

--- App.tsx
+++ App.tsx
@@ -1,23 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigator from './navigation/Navigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    <Text>Ish&apos;s Branch</Text>
-      <Text> Ethan&apos;s branch</Text>
-      <StatusBar />
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
