b192bee7a7de76c6aeefa5effd4e82a840431d28
Ish <ishrat@cec.sc.edu>
2021-11-07 22:07:19 -0500

    update exercisesSlice

--- redux/slices/exercisesSlice.ts
+++ redux/slices/exercisesSlice.ts
@@ -1,24 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable import/no-cycle */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import Exercise from '../../models/Exercise';
+import { RootState } from '../store';
+
+interface ExercisesState {
+  exercises: Exercise[];
+}
+
+const initialState: ExercisesState = {
+  exercises: [],
+};
 
 const exercisesSlice = createSlice({
   name: 'exercises',
-  initialState: {
-    exercises: <Exercise[]>[],
-  },
+  initialState,
   reducers: {
-    addExercise: (state, action) => {
-      const exercise: Exercise = action.payload;
-      state.exercises = [...state.exercises, exercise];
+    addExercise: (state, action: PayloadAction<Exercise>) => {
+      state.exercises = [...state.exercises, action.payload];
     },
-    deleteExercise: (state, action) => {
-      const exercise: Exercise = action.payload;
-      state.exercises = state.exercises.filter((e) => e.id !== exercise.id);
+    deleteExercise: (state, action: PayloadAction<Exercise>) => {
+      state.exercises = state.exercises.filter((e) => e.id !== action.payload.id);
     },
   },
 });
 
 export const { addExercise, deleteExercise } = exercisesSlice.actions;
+
+export const selectExercises = (state: RootState) => state.exercises.exercises;
 
 const exercisesReducer = exercisesSlice.reducer;
 export default exercisesReducer;
