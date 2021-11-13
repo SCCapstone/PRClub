df21302278575a3110142d03572005172d5572f8
Ish <ishrat@cec.sc.edu>
2021-11-13 18:51:13 -0500

    add validation to form

--- components/CreateWorkoutForm.tsx
+++ components/CreateWorkoutForm.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
-import {
-  Field, FieldArray, Formik,
-} from 'formik';
+import * as yup from 'yup';
+import { Field, FieldArray, Formik } from 'formik';
 import { Button, Input, Text } from 'react-native-elements';
 import { View } from 'react-native';
 import tw from 'twrnc';
 import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
 
-interface ExerciseSetInput {
-  weight: number,
-  reps: number,
-}
-interface ExerciseInput {
-  name: string,
-  exerciseSets: ExerciseSetInput[],
-}
-interface WorkoutInput {
-  name: string,
-  exercises: ExerciseInput[],
-}
+// #region form validation schemas
+const ExerciseSetInputSchema = yup.object({
+  reactKey: yup.string()
+    .matches(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+    .required(), // uuidv4 string
+  weight: yup.lazy(
+    (val) => (typeof val === 'number' && Number.isFinite(val)
+      ? yup.number().positive().required()
+      : yup.string().matches(/^$/)).required(),
+  ), // number or empty string
+  reps: yup.lazy(
+    (val) => (typeof val === 'number' && Number.isInteger(val)
+      ? yup.number().positive().integer().required()
+      : yup.string().matches(/^$/)).required(),
+  ), // number or empty string
+});
+type ExerciseSetInput = yup.InferType<typeof ExerciseSetInputSchema>;
 
-const initialValues = {
-  name: '',
-  exercises: [] as ExerciseInput[],
-} as WorkoutInput;
+const ExerciseInputSchema = yup.object({
+  reactKey: yup.string()
+    .matches(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+    .required(), // uuidv4 string
+  name: yup.string().min(1).required(),
+  exerciseSets: yup.array(ExerciseSetInputSchema).min(1).required(),
+});
+type ExerciseInput = yup.InferType<typeof ExerciseInputSchema>;
+
+const WorkoutInputSchema = yup.object({
+  name: yup.string().min(1).required(),
+  exercises: yup.array(ExerciseInputSchema).min(1).required(),
+});
+type WorkoutInput = yup.InferType<typeof WorkoutInputSchema>;
+// #endregion
 
 export default function CreateWorkoutForm() {
   return (
     <Formik
-      initialValues={initialValues}
-      onSubmit={(values) => console.log(values)}
+      initialValues={{
+        name: '',
+        exercises: [] as ExerciseInput[],
+      } as WorkoutInput}
+      validationSchema={WorkoutInputSchema}
+      onSubmit={(values) => {
+        console.log(values); // TODO: parse form submission data and send to workoutsReducer
+      }}
     >
       {(formikProps) => (
         <View>
@@ -44,8 +66,8 @@ export default function CreateWorkoutForm() {
                 <>
                   {formikProps.values.exercises
                     && formikProps.values.exercises.length > 0 ? (
-                      formikProps.values.exercises.map((_, i) => (
-                        <View key={i} style={tw`bg-gray-300 p-3`}>
+                      formikProps.values.exercises.map((exercise, i) => (
+                        <View key={exercise.reactKey} style={tw`bg-gray-300 p-3`}>
                           <View style={tw`flex flex-row`}>
                             <View style={tw`flex flex-3`}>
                               <Field name={`exercises.${i}.name`}>
@@ -87,55 +109,67 @@ export default function CreateWorkoutForm() {
                                 {formikProps.values.exercises[i]
                                   && formikProps.values.exercises[i].exerciseSets
                                   && formikProps.values.exercises[i].exerciseSets.length > 0 ? (
-                                    formikProps.values.exercises[i].exerciseSets.map((__, j) => (
-                                      <View key={j} style={tw`flex flex-row`}>
-                                        <View style={tw`flex flex-1`}>
-                                          <Text style={tw`text-center text-xl`}>{j + 1}</Text>
+                                    formikProps.values.exercises[i].exerciseSets.map(
+                                      (exerciseSet, j) => (
+                                        <View key={exerciseSet.reactKey} style={tw`flex flex-row`}>
+                                          <View style={tw`flex flex-1`}>
+                                            <Text style={tw`text-center text-xl`}>{j + 1}</Text>
+                                          </View>
+                                          <View style={tw`flex flex-3`}>
+                                            <Field name={`exercises.${i}.exerciseSets.${j}.weight`}>
+                                              {() => (
+                                                <Input
+                                                  placeholder="weight (lbs)"
+                                                  onChangeText={(input) => {
+                                                    formikProps.setFieldValue(
+                                                      `exercises.${i}.exerciseSets.${j}.weight`,
+                                                      Number(input),
+                                                    );
+                                                  }}
+                                                  value={String(formikProps
+                                                    .values
+                                                    .exercises[i]
+                                                    .exerciseSets[j]
+                                                    .weight)}
+                                                  keyboardType="decimal-pad"
+                                                />
+                                              )}
+                                            </Field>
+                                          </View>
+                                          <View style={tw`flex flex-3`}>
+                                            <Field name={`exercises.${i}.exerciseSets.${j}.reps`}>
+                                              {() => (
+                                                <Input
+                                                  placeholder="reps"
+                                                  onChangeText={(input) => {
+                                                    formikProps.setFieldValue(
+                                                      `exercises.${i}.exerciseSets.${j}.reps`,
+                                                      Number(input),
+                                                    );
+                                                  }}
+                                                  value={String(formikProps
+                                                    .values
+                                                    .exercises[i]
+                                                    .exerciseSets[j]
+                                                    .reps)}
+                                                  keyboardType="numeric"
+                                                />
+                                              )}
+                                            </Field>
+                                          </View>
+                                          <View style={tw`flex flex-1`}>
+                                            <Button
+                                              icon={{
+                                                name: 'delete',
+                                                color: 'white',
+                                              }}
+                                              buttonStyle={tw`bg-red-500`}
+                                              onPress={() => exerciseSetsHelpers.remove(j)}
+                                            />
+                                          </View>
                                         </View>
-                                        <View style={tw`flex flex-3`}>
-                                          <Field name={`exercises.${i}.exerciseSets.${j}.weight`}>
-                                            {() => (
-                                              <Input
-                                                placeholder="weight (lbs)"
-                                                onChangeText={formikProps.handleChange(`exercises.${i}.exerciseSets.${j}.weight`)}
-                                                value={String(formikProps
-                                                  .values
-                                                  .exercises[i]
-                                                  .exerciseSets[j]
-                                                  .weight)}
-                                                keyboardType="decimal-pad"
-                                              />
-                                            )}
-                                          </Field>
-                                        </View>
-                                        <View style={tw`flex flex-3`}>
-                                          <Field name={`exercises.${i}.exerciseSets.${j}.reps`}>
-                                            {() => (
-                                              <Input
-                                                placeholder="reps"
-                                                onChangeText={formikProps.handleChange(`exercises.${i}.exerciseSets.${j}.reps`)}
-                                                value={String(formikProps
-                                                  .values
-                                                  .exercises[i]
-                                                  .exerciseSets[j]
-                                                  .reps)}
-                                                keyboardType="numeric"
-                                              />
-                                            )}
-                                          </Field>
-                                        </View>
-                                        <View style={tw`flex flex-1`}>
-                                          <Button
-                                            icon={{
-                                              name: 'delete',
-                                              color: 'white',
-                                            }}
-                                            buttonStyle={tw`bg-red-500`}
-                                            onPress={() => exerciseSetsHelpers.remove(j)}
-                                          />
-                                        </View>
-                                      </View>
-                                    ))
+                                      ),
+                                    )
                                   ) : <></>}
                                 <Button
                                   title="add set"
@@ -145,9 +179,10 @@ export default function CreateWorkoutForm() {
                                   }}
                                   buttonStyle={tw`bg-green-500`}
                                   onPress={() => exerciseSetsHelpers.push({
+                                    reactKey: uuidv4(),
                                     weight: '',
                                     reps: '',
-                                  })}
+                                  } as ExerciseSetInput)}
                                 />
                               </View>
                             )}
@@ -165,9 +200,10 @@ export default function CreateWorkoutForm() {
                     }}
                     buttonStyle={tw`bg-green-500`}
                     onPress={() => exercisesHelpers.push({
+                      reactKey: uuidv4(),
                       name: '',
                       exerciseSets: [],
-                    })}
+                    } as ExerciseInput)}
                   />
                 </View>
               </>
--- redux/store.ts
+++ redux/store.ts
@@ -1,14 +1,10 @@
 /* eslint-disable import/no-cycle */
 import { configureStore } from '@reduxjs/toolkit';
-import exerciseSetsReducer from './slices/exerciseSetsSlice';
-import exercisesReducer from './slices/exercisesSlice';
 import workoutsReducer from './slices/workoutsSlice';
 
 const store = configureStore({
   reducer: {
     workouts: workoutsReducer,
-    exercises: exercisesReducer,
-    exerciseSets: exerciseSetsReducer,
   },
 });
 
