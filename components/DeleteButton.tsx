e8d471111f9b9c3b2bb1087c8a36e474310941c1
Ish <ishrat@cec.sc.edu>
2022-01-26 18:36:32 -0500

    remove unused imports

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
 import { Button } from 'react-native-paper';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import tw from 'twrnc';
 
 export default function DeleteButton({ onPress }: {onPress: () => void}) {
   return (
