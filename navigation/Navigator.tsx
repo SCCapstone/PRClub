957038cfd395bf811064e30dcdff757f7b81dea0
eschim <eschimelman@gmail.com>
2022-01-26 14:22:40 -0500

    Initial Nav Refactor

--- navigation/Navigator.tsx
+++ navigation/Navigator.tsx
@@ -1,46 +1,33 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import WorkoutsScreen from '../screens/WorkoutsScreen';
-import CreateWorkoutScreen from '../screens/CreateWorkoutScreen';
+import Login from '../components/Login';
+import Tabs from './Tabs';
 
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function Navigator() {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName = '';
+    <Stack.Navigator>
 
-          if (route.name === 'Workouts') {
-            iconName = focused ? 'barbell' : 'barbell-outline';
-          } else if (route.name === 'Create Workout') {
-            iconName = focused ? 'add-circle' : 'add-circle-outline';
-          } /* else if (route.name === 'Profile') {
-            iconName = focused ? 'person-circle' : 'person-circle-outline';
-          } */
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          title: 'Sign in',
+          // When logging out, a pop animation feels intuitive
+          // animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+        }}
+      />
 
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      })}
-    >
-      <Tab.Screen
-        name="Create Workout"
-        component={CreateWorkoutScreen}
+      <Stack.Screen
+        name="logged in"
+        component={Tabs}
+        options={{
+          title: 'Signed in',
+          // When logging out, a pop animation feels intuitive
+          // animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+        }}
       />
-      <Tab.Screen
-        name="Workouts"
-        component={WorkoutsScreen}
-      />
-      {/*
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-      /> */}
-    </Tab.Navigator>
+    </Stack.Navigator>
   );
 }
