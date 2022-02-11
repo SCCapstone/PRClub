b16a21f1d42a0836047e19bd207138ecc17d452a
singhish <ishrat@cec.sc.edu>
2022-02-11 15:33:20 -0500

    basic post infra

--- components/Workouts.tsx
+++ components/Workouts.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import {
-  View, ScrollView,
-} from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { ActivityIndicator, Text } from 'react-native-paper';
 import tw from 'twrnc';
 import useAppDispatch from '../hooks/useAppDispatch';
@@ -11,9 +9,8 @@ import { selectWorkoutsSortedByMostRecent, selectWorkoutsStatus } from '../state
 import Workout from '../types/shared/Workout';
 import { SliceStatus } from '../types/state/SliceStatus';
 import CancelButton from './BackButton';
-import DeleteButton from './DeleteButton';
-import EditButton from './EditButton';
 import WorkoutForm from './WorkoutForm';
+import WorkoutItem from './WorkoutItem';
 
 export default function Workouts() {
   const workouts: Workout[] = useAppSelector(selectWorkoutsSortedByMostRecent);
@@ -75,59 +72,16 @@ export default function Workouts() {
               </View>
             )
             : workouts.map((workout) => (
-              <View key={workout.id} style={tw`rounded overflow-hidden shadow-lg m-2 p-2`}>
-                <View style={tw`flex flex-row`}>
-                  <View style={tw`flex flex-4`}>
-                    {
-                      workout.modifiedDate
-                        ? <Text style={tw`italic`}>{`Edited ${new Date(workout.modifiedDate).toLocaleString()}`}</Text>
-                        : <></>
-                    }
-                    <Text>{`On ${new Date(workout.createdDate).toLocaleString()}:`}</Text>
-                    <Text style={tw`font-bold text-base`}>{workout.name}</Text>
-                  </View>
-                  <View style={tw`flex flex-1 p-2`}>
-                    <EditButton onPress={() => {
-                      setWorkoutToEdit(workout);
-                      toggleEditingWorkout();
-                    }}
-                    />
-                  </View>
-                  <View style={tw`flex flex-1 p-2`}>
-                    <DeleteButton onPress={() => dispatch(removeWorkoutByEntity(workout))} />
-                  </View>
-                </View>
-                <View>
-                  {workout.exercises.map((exercise) => (
-                    <View key={exercise.id} style={tw`bg-gray-300 p-3`}>
-                      <Text style={tw`font-bold text-base text-center`}>
-                        {exercise.name}
-                      </Text>
-                      <View style={tw`bg-gray-400 p-3`}>
-                        <View style={tw`flex flex-row`}>
-                          <View style={tw`flex flex-1`}>
-                            <Text style={tw`text-center font-bold`}>Weight</Text>
-                          </View>
-                          <View style={tw`flex flex-1`}>
-                            <Text style={tw`text-center font-bold`}>Reps</Text>
-                          </View>
-                        </View>
-                        {exercise.exerciseSets.map((set) => (
-                          <View key={set.id} style={tw`flex flex-row`}>
-                            <View style={tw`flex flex-1`}>
-                              <Text style={tw`text-center font-bold`}>{set.weight}</Text>
-                            </View>
-                            <View style={tw`flex flex-1`}>
-                              <Text style={tw`text-center font-bold`}>{set.reps}</Text>
-                            </View>
-                          </View>
-                        ))}
-                      </View>
-
-                    </View>
-                  ))}
-                </View>
-              </View>
+              <WorkoutItem
+                key={workout.id}
+                workout={workout}
+                onEdit={() => {
+                  setWorkoutToEdit(workout);
+                  toggleEditingWorkout();
+                }}
+                onDelete={() => dispatch(removeWorkoutByEntity(workout))}
+                onShare={() => console.log('shared post')}
+              />
             ))}
         </ScrollView>
       </View>
---
+++ types/shared/Post.ts
@@ -0,0 +1,6 @@
+export default interface Post {
+  id: string;
+  userId: string;
+  workoutId: string;
+  text: string;
+}
