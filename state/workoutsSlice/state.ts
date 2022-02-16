1eca0968e240f09a15a17c562fb18a8c12d75edf
NickYglesias64 <47831431+NickYglesias64@users.noreply.github.com>
2022-02-15 19:58:06 -0500

    Update state.ts

--- state/workoutsSlice/state.ts
+++ state/workoutsSlice/state.ts
@@ -1,13 +1,23 @@
-import { createEntityAdapter } from '@reduxjs/toolkit';
+import { createEntityAdapter, SerializedError } from '@reduxjs/toolkit';
 import { SliceStatus } from '../../types/state/SliceStatus';
 import Workout from '../../types/shared/Workout';
 
 interface WorkoutsInitialState {
   status: SliceStatus
+  workoutsServiceUpsertResult: ServiceCallResult | null
+  workoutsServiceRemoveResult: ServiceCallResult | null
+}
+
+interface ServiceCallResult {
+  success: boolean
+  error?: SerializedError
 }
 
 export const workoutsAdapter = createEntityAdapter<Workout>();
 
 export const initialState = workoutsAdapter.getInitialState<WorkoutsInitialState>({
   status: 'idle',
+  workoutsServiceUpsertResult: null,
+  workoutsServiceRemoveResult: null,
+
 });
