9c42b75b2bcde102c8194156e43b14a76fdbf342
singhish <ishrat@cec.sc.edu>
2022-02-19 20:08:43 -0500

    refactor services to avoid manual exports

--- services/UsersService.ts
+++ services/UsersService.ts
@@ -7,101 +7,96 @@ import { USERS_COLLECTION } from '../constants/firestore';
 import { db } from '../firebase';
 import User from '../types/shared/User';
 
-async function getUsersByIds(userIds: string[]): Promise<User[]> {
-  if (userIds.length === 0) {
-    return [];
-  }
-
-  const q = query(
-    collection(db, USERS_COLLECTION),
-    where('id', 'in', userIds),
-  );
-
-  const querySnap = await getDocs(q);
-
-  const users: User[] = [];
-  querySnap.forEach((u) => {
-    users.push(u.data() as User);
-  });
-
-  return users;
-}
-
-async function getUsersByQuery(queryString: string): Promise<User[]> {
-  if (queryString === '') {
-    return [];
-  }
-
-  // query by substring using string comparisons
-  const q1 = query(
-    collection(db, USERS_COLLECTION),
-    where('username', '>=', queryString),
-    // append PUA unicode character to upper range to catch all matching substrings
-    where('username', '<=', `${queryString}\uf8ff`),
-  );
-  const q2 = query(
-    collection(db, USERS_COLLECTION),
-    where('name', '>=', queryString),
-    where('name', '<=', `${queryString}\uf8ff`),
-  );
-
-  const usernameQuerySnap = await getDocs(q1);
-  const nameQuerySnap = await getDocs(q2);
-
-  const users: User[] = [];
-  usernameQuerySnap.forEach((u) => {
-    users.push(u.data() as User);
-  });
-  nameQuerySnap.forEach((u) => {
-    if (!users.map((us) => us.id).includes(u.id)) {
-      users.push(u.data() as User);
-    }
-  });
-
-  return users;
-}
-
 async function checkUserExists(userDocRef: DocumentReference<DocumentData>): Promise<void> {
   if (!(await getDoc(userDocRef)).exists()) {
     throw new Error(`User with id ${userDocRef.id} doesn't exist!`);
   }
 }
 
-async function createFollowerRelationship(userId: string, userToFollowId: string): Promise<void> {
-  const userDocRef = doc(db, USERS_COLLECTION, userId);
-  checkUserExists(userDocRef);
-
-  const userToFollowDocRef = doc(db, USERS_COLLECTION, userToFollowId);
-  checkUserExists(userToFollowDocRef);
-
-  await updateDoc(userDocRef, {
-    followingIds: arrayUnion(userToFollowId),
-  });
-
-  await updateDoc(userToFollowDocRef, {
-    followerIds: arrayUnion(userId),
-  });
-}
-
-async function removeFollowerRelationship(userId: string, userToFollowId: string): Promise<void> {
-  const userDocRef = doc(db, USERS_COLLECTION, userId);
-  checkUserExists(userDocRef);
-
-  const userToFollowDocRef = doc(db, USERS_COLLECTION, userToFollowId);
-  checkUserExists(userToFollowDocRef);
-
-  await updateDoc(userDocRef, {
-    followingIds: arrayRemove(userToFollowId),
-  });
-
-  await updateDoc(userToFollowDocRef, {
-    followerIds: arrayRemove(userId),
-  });
-}
-
 export default {
-  getUsersByIds,
-  getUsersByQuery,
-  createFollowerRelationship,
-  removeFollowerRelationship,
+  async getUsersByIds(userIds: string[]): Promise<User[]> {
+    if (userIds.length === 0) {
+      return [];
+    }
+
+    const q = query(
+      collection(db, USERS_COLLECTION),
+      where('id', 'in', userIds),
+    );
+
+    const querySnap = await getDocs(q);
+
+    const users: User[] = [];
+    querySnap.forEach((u) => {
+      users.push(u.data() as User);
+    });
+
+    return users;
+  },
+
+  async getUsersByQuery(queryString: string): Promise<User[]> {
+    if (queryString === '') {
+      return [];
+    }
+
+    // query by substring using string comparisons
+    const q1 = query(
+      collection(db, USERS_COLLECTION),
+      where('username', '>=', queryString),
+      // append PUA unicode character to upper range to catch all matching substrings
+      where('username', '<=', `${queryString}\uf8ff`),
+    );
+    const q2 = query(
+      collection(db, USERS_COLLECTION),
+      where('name', '>=', queryString),
+      where('name', '<=', `${queryString}\uf8ff`),
+    );
+
+    const usernameQuerySnap = await getDocs(q1);
+    const nameQuerySnap = await getDocs(q2);
+
+    const users: User[] = [];
+    usernameQuerySnap.forEach((u) => {
+      users.push(u.data() as User);
+    });
+    nameQuerySnap.forEach((u) => {
+      if (!users.map((us) => us.id).includes(u.id)) {
+        users.push(u.data() as User);
+      }
+    });
+
+    return users;
+  },
+
+  async createFollowerRelationship(userId: string, userToFollowId: string): Promise<void> {
+    const userDocRef = doc(db, USERS_COLLECTION, userId);
+    checkUserExists(userDocRef);
+
+    const userToFollowDocRef = doc(db, USERS_COLLECTION, userToFollowId);
+    checkUserExists(userToFollowDocRef);
+
+    await updateDoc(userDocRef, {
+      followingIds: arrayUnion(userToFollowId),
+    });
+
+    await updateDoc(userToFollowDocRef, {
+      followerIds: arrayUnion(userId),
+    });
+  },
+
+  async removeFollowerRelationship(userId: string, userToFollowId: string): Promise<void> {
+    const userDocRef = doc(db, USERS_COLLECTION, userId);
+    checkUserExists(userDocRef);
+
+    const userToFollowDocRef = doc(db, USERS_COLLECTION, userToFollowId);
+    checkUserExists(userToFollowDocRef);
+
+    await updateDoc(userDocRef, {
+      followingIds: arrayRemove(userToFollowId),
+    });
+
+    await updateDoc(userToFollowDocRef, {
+      followerIds: arrayRemove(userId),
+    });
+  },
 };
