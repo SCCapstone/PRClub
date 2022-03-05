60ac46c17b99fa1113bd513fb72618862671b44b
singhish <ishrat@cec.sc.edu>
2022-03-04 20:06:25 -0500

    configure firebase emulators locally

--- firebase-lib/index.ts
+++ firebase-lib/index.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from '@firebase/app';
-import { getAuth } from '@firebase/auth';
-import { getFirestore } from '@firebase/firestore';
-import { getStorage } from '@firebase/storage';
+import { connectAuthEmulator, getAuth } from '@firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from '@firebase/firestore';
+import { connectStorageEmulator, getStorage } from '@firebase/storage';
 import Constants from 'expo-constants';
 
 if (!Constants.manifest) {
@@ -19,3 +19,9 @@ export const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+
+if (__DEV__) {
+  connectFirestoreEmulator(firestore, 'localhost', 8080);
+  connectAuthEmulator(auth, 'http://localhost:9099');
+  connectStorageEmulator(storage, 'localhost', 9199);
+}
