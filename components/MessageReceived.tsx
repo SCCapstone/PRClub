52fc94587a14a72ec898f63258d955ed1cc78dc4
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2022-03-30 00:35:15 -0400

    more styling

--- components/MessageReceived.tsx
+++ components/MessageReceived.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 
 export default function MessageReceived({ message }: {message:string}) {
   return (
-    <View style={tw`bg-[#8e8e93] m-2 p-4 w-1/4 rounded-md`}>
+    <View style={tw`bg-[#8e8e93] m-2 p-4 max-w-xs rounded-md`}>
       <Text>{message}</Text>
     </View>
   );
--- components/MessageSent.tsx
+++ components/MessageSent.tsx
@@ -5,7 +5,7 @@ import { View, Text } from 'react-native';
 export default function MessageSent({ message }: {message:string}) {
   return (
     <View>
-      <View style={tw`bg-[#147efb] m-2 p-4 w-1/4 ml-auto rounded-md`}>
+      <View style={tw`bg-[#147efb] m-2 p-4 max-w-xs ml-auto rounded-md`}>
         <Text style={tw`text-white`}>{message}</Text>
       </View>
     </View>
