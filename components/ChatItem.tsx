fea4ae89124dd7a76e44e2b27a2a398d7ce8fb14
johnangelidis <59876191+johnangelidis@users.noreply.github.com>
2022-03-30 01:20:57 -0400

    chat item displays username instead of id

--- components/ChatItem.tsx
+++ components/ChatItem.tsx
@@ -3,24 +3,34 @@ import tw from 'twrnc';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { ref } from '@firebase/database';
 import { useDatabaseListData } from 'reactfire';
-import { database } from '../firebase-lib';
+import { doc, getDoc } from '@firebase/firestore';
+import { database, firestore } from '../firebase-lib';
 import { useAppSelector } from '../hooks/redux';
 import { selectCurrentUser } from '../state/userSlice/selectors';
 import Chat from './Chat';
+import { USERS_COLLECTION } from '../constants/firestore';
+import User from '../models/firestore/User';
 
 export default function ChatItem({ chatId }:
 {chatId: string}) {
   // console.log(chatId);
+  // for some reason, the chatId with all messages does not exist in chats or users in realtime db so im hardcoding it
   const tempChatId = '-MzMEKa0YcQsy2RWaC4O';
   const currentUser = useAppSelector(selectCurrentUser);
   if (!currentUser) {
     return <></>;
   }
-  const [senderId, setSenderId] = useState('');
+  const [senderId, setSenderId] = useState<string>('');
+  const [senderUsername, setSenderUsername] = useState<string>('');
   // get senderID
 
   const chatsRef = ref(database, `chats/${chatId}`);
   const { data: chats } = useDatabaseListData(chatsRef);
+  const messagesRef = ref(database, `messages/${tempChatId}`);
+  const { status, data: msgs } = useDatabaseListData(messagesRef);
+  const messageLength = msgs?.length;
+  const lastMessage = msgs?.at(messageLength - 1)?.message;
+  const [chatClicked, setChatClicked] = useState<boolean>(false);
   useEffect(() => {
     chats?.forEach((chat) => {
       const members = Object.keys(chat);
@@ -29,13 +39,19 @@ export default function ChatItem({ chatId }:
         if (id !== 'NO_ID_FIELD' && currentUser.id !== id) { setSenderId(id); }
       });
     });
+    const getUsernameFromId = async (userId:string) => {
+      const userDocRef = doc(firestore, USERS_COLLECTION, userId);
+      const userDocSnap = await getDoc(userDocRef);
+
+      if (!userDocSnap.exists()) {
+        return null;
+      }
+
+      const user = userDocSnap.data() as User;
+      return user.username;
+    };
+    getUsernameFromId(senderId).then((res) => setSenderUsername(res as string));
   });
-  // for some reason, the chatId with all messages does not exist in chats or users in realtime db so im hardcoding it
-  const messagesRef = ref(database, `messages/${tempChatId}`);
-  const { status, data: msgs } = useDatabaseListData(messagesRef);
-  const messageLength = msgs?.length;
-  const lastMessage = msgs?.at(messageLength - 1)?.message;
-  const [chatClicked, setChatClicked] = useState<boolean>(false);
 
   return (
 
@@ -44,7 +60,7 @@ export default function ChatItem({ chatId }:
         setChatClicked(!chatClicked);
       }}
       >
-        <Text style={tw`font-bold`}>{senderId}</Text>
+        <Text style={tw`font-bold`}>{senderUsername}</Text>
         <Text style={tw`text-sm text-gray-800`}>{lastMessage}</Text>
       </TouchableOpacity>
       {chatClicked && <Chat chatId={tempChatId} />}
