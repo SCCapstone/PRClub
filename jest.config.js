c40e6ab7a2d5f9f8826214d6f3cdac94a0f25bb8
singhish <ishrat@cec.sc.edu>
2022-04-18 15:32:13 -0400

    add tests for wgerservice

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,6 @@ module.exports = {
   preset: "jest-expo",
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
-  ]
+  ],
+  testTimeout: 300000
 };
