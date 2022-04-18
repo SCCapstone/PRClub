4b94127f4397f0a3054723df3d23afa32d5053b4
NickYglesias64 <47831431+NickYglesias64@users.noreply.github.com>
2022-04-18 16:22:28 -0400

    Cleaned up prs-service.test

--- services/__tests__/prs-service.test.ts
+++ services/__tests__/prs-service.test.ts
@@ -14,15 +14,10 @@ describe('PRsService', () => {
       name: 'Em2',
       exerciseName: '',
     };
-
-    const exercise1 = {
+    // Exercise from emulator
+    const exercise = {
       exerciseName: 'Incline Plank With Alternate Floor Touch',
       volume: 820,
-    };
-
-    const exercise2 = {
-      exerciseName: 'Incline Plank With Alternate Floor Touch',
-      volume: 850,
     };
 
     const workout: Workout = {
@@ -35,60 +30,32 @@ describe('PRsService', () => {
       exercises: [],
     };
 
-    /*
-      -mocking user. in order to replicate creating a pr, must have new workout
-       of higher volume
-      - create exercise with workout youre trying to create a pr for
-      -initialize this workout with higher volume than previous
-      -upsert workout
-      -TEST 1 pr doc should automatically update
-      -remove PR function on this PR
-      -TEST 2 check if PR doc is good
-    */
-
     // Mock PR object using latest workout of Em2
-    const pr1: PR = {
+    const pr: PR = {
       id: uuidv4(),
       userId: user.id,
       username: user.name,
       date: new Date().toString(),
       workoutId: workout.id,
-      exerciseName: exercise1.exerciseName,
-      volume: exercise1.volume,
-    };
-    const pr2: PR = {
-      id: uuidv4(),
-      userId: user.id,
-      username: user.name,
-      date: new Date().toString(),
-      workoutId: workout.id,
-      exerciseName: exercise2.exerciseName,
-      volume: exercise2.volume,
+      exerciseName: exercise.exerciseName,
+      volume: exercise.volume,
     };
 
-    const prs1:PR[] = [pr1];
-    const prs2:PR[] = [pr2];
+    // Create PRs array and get PR Document
+    const prs:PR[] = [pr];
+    const prDoc = doc(firestore, PRS_COLLECTION, pr.id);
 
-    const pr1Doc = doc(firestore, PRS_COLLECTION, pr1.id);
-    const pr2Doc = doc(firestore, PRS_COLLECTION, pr2.id);
     // attempt to add new PR
-    await PRsService.upsertPRs(prs1);
+    await PRsService.upsertPRs(prs);
 
     // query firestore to get the pr doc and check firestore
-    const upsertedPr1Data = await getDoc(pr1Doc);
-    const upsertedPr = upsertedPr1Data.data() as PR;
-    expect(upsertedPr).toEqual(pr1);
-
-    await PRsService.upsertPRs(prs2);
-    const upsertedPr2Data = await getDoc(pr2Doc);
-    const upsertedPr2 = upsertedPr2Data.data() as PR;
-    expect(upsertedPr2).toEqual(pr2);
+    const upsertedPrData = await getDoc(prDoc);
+    const upsertedPr = upsertedPrData.data() as PR;
+    expect(upsertedPr).toEqual(pr);
 
     // remove PR and make sure it got deleted
-    await PRsService.removePRs(prs2);
-    const deletedPr1Data = await getDoc(pr1Doc);
-    const deletedPr2Data = await getDoc(pr2Doc);
-    expect(deletedPr1Data.exists()).toBe(false);
-    expect(deletedPr2Data.exists()).toBe(false);
+    await PRsService.removePRs(prs);
+    const deletedPrData = await getDoc(prDoc);
+    expect(deletedPrData.exists()).toBe(false);
   });
 });
